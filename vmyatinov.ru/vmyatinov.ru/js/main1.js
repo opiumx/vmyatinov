a0405a30b372ac3453ea91116d5c974cfa7b2d75
zengang <pidlisnyid@gmail.com>
2015-01-16 15:03:09 +0200

    добавляем цветовой эффект при наведениия на фото сотрудника

--- vmyatinov.ru/vmyatinov.ru/js/main1.js
+++ vmyatinov.ru/vmyatinov.ru/js/main1.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
+
+	//adding file button to popup
+	//-----------------------------------------------------------------------------------------
 	$('.add-more').on('click', function(){
 		$(this).prev().append($('<div class="adding-btn-item"><input type="file"/></div>')).append('  ');
 	})
+
+	//photo change color
+	//-----------------------------------------------------------------------------------------
+	$('.people-box').BlackAndWhite({
+		hoverEffect : true,
+		webworkerPath : false,
+		invertHoverEffect: false,
+		intensity:0,
+		speed: {
+			fadeIn: 200,
+			fadeOut: 800
+		},
+		onImageReady:function(img) {
+
+		}
+	});
+
+
 });
